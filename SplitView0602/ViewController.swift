d82a18d1ebb74c9510e3d4ac5c15682bf6bb5b29
Leslie <leslie.appl@gmail.com>
2020-06-02 13:56:00 +0800

    Initial Commit

---
+++ SplitView0602/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitView0602
+//
+//  Created by leslie on 6/2/20.
+//  Copyright © 2020 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
